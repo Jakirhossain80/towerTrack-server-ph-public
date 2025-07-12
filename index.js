3ee1d1acf2016e6e1df65222d7d1097f49d316eb
JakirHossain <jakir.devbd@gmail.com>
2025-07-13 01:54:05 +0600

    Some necessary packages have installed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
